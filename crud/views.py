1b0ec2d99c9200a02b867cb0e51b054521d119f7
AmodPradhan <np03cs4s220027@heraldcollege.edu.np>
2023-07-18 08:52:03 +0545

    migrations added

--- crud/views.py
+++ crud/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blog    # object
 
 # Create your views here.
 def index(request):
+    blog = Blog.objects.all()
     # return HttpResponse("Hello this is form app")
-    return render(request,"crud/index.html")
+    return render(request,"crud/index.html",
+                  {"blogs":blog}) # calling from above blog variable and :(putting) it to loop in index.html (templates)
 
 def about(request):
     return render(request,"crud/about.html")
